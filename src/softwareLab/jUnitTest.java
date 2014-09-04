01113cc0e8c4288e724058637d45eb26cb63f1ba
Shou <Shou@192.168.18.167>
2014-09-04 12:44:46 +0530

    added a comment

---
+++ src/softwareLab/jUnitTest.java
@@ -0,0 +1,30 @@
+package softwareLab;
+
+public class jUnitTest 
+{
+	
+	
+	public jUnitTest()
+	{
+		
+	}
+	//added a comment
+	public int mult(int a, int b)
+	{
+		int c;
+		c = a*b;
+		return c;
+	}
+	
+	public int sum(int a, int b)
+	{
+		int c;
+		c= a+b;
+		return c;
+	}
+	public static void main(String args[])
+	{
+		jUnitTest jst = new jUnitTest();
+		
+	}
+}
